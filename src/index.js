7b9243159a9c5a588e80bda1bb4c537678bb7fe1
Ihor Komar <kisenbenz8@gmail.com>
2022-09-22 17:19:03 +0300

    js-11

--- src/index.js
+++ src/index.js
@@ -4,25 +4,19 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import LoadMoreBtn from './js/load-more-button';
 
-// На завтра : 1. пофиксить консоль лог (ошибка)
-
 const refs = {
   galleryWrap: document.querySelector('.gallery'),
   form: document.querySelector('#search-form'),
   loadMoreBtn: document.querySelector('.load-more'),
   endText: document.querySelector('.end__text'),
-  // btn_anchor: document.querySelector('.button-anchor'),
 };
 
-// console.log(refs.btn_anchor);
 const loadMoreBtn = new LoadMoreBtn({
   cls: '.load-more',
   hidden: true,
 });
 const galleryApiService = new GalleryApiService();
 let lightbox = {};
-
-// console.log(loadMoreBtn);
 
 refs.form.addEventListener('submit', onSearch);
 if (loadMoreBtn.refs.btn) {
@@ -36,15 +30,13 @@ async function onSearch(e) {
   galleryApiService.searchQuery =
     e.currentTarget.elements.searchQuery.value.trim();
 
-  // console.log(galleryApiService.searchQuery);
-
   if (!galleryApiService.searchQuery) {
     clearGalleryMarkup();
     Notify.warning('Please write something');
     loadMoreBtn.hide();
     refs.endText.classList.add('is-hidden');
     refs.form.reset();
-    // refs.btn_anchor.classList.add('is-hidden');
+
     return;
   }
 
@@ -74,14 +66,13 @@ function onSubmitControl(filesFromBackEnd) {
       `Hooray! We found ${filesFromBackEnd.data.total} images, but we can only show the first ${filesFromBackEnd.data.totalHits}!`
     );
     loadMoreBtn.show();
-    // refs.btn_anchor.classList.remove('is-hidden');
   } else if (
     filesFromBackEnd.data.total > 40 &&
     filesFromBackEnd.data.total <= 500
   ) {
     Notify.success(`Hooray! We found ${filesFromBackEnd.data.total} images!`);
     loadMoreBtn.show();
-    // refs.btn_anchor.classList.remove('is-hidden');
+
     refs.endText.classList.add('is-hidden');
   } else if (
     filesFromBackEnd.data.totalHits > 0 &&
@@ -90,18 +81,15 @@ function onSubmitControl(filesFromBackEnd) {
     Notify.success(`Hooray! We found ${filesFromBackEnd.data.total} images!`);
     loadMoreBtn.hide();
     refs.endText.classList.remove('is-hidden');
-    // refs.btn_anchor.classList.remove('is-hidden');
   } else if (filesFromBackEnd.data.total === 0) {
     Notify.failure(
       'Sorry, there are no images matching your search query. Please try again.'
     );
     loadMoreBtn.hide();
-    // refs.btn_anchor.classList.add('is-hidden');
   }
 }
 
 function createGalleryMarkup(cards) {
-  // console.log(cards);
   refs.galleryWrap.insertAdjacentHTML(
     'beforeend',
     cards
@@ -145,7 +133,7 @@ function clearGalleryMarkup() {
 async function onLoadMore() {
   const filesFromBackEnd = await galleryApiService.fetchGallery();
   createGalleryMarkup(filesFromBackEnd.data.hits);
-  // console.log(filesFromBackEnd.data.hits);
+
   if (
     filesFromBackEnd.data.hits.length >= 0 &&
     filesFromBackEnd.data.hits.length < 40
